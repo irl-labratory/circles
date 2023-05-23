bad01ae80b858a947546b829dca4adb9f5a5fd84
Nicholas <lynick14@gmail.com>
2023-05-23 17:00:03 -0400

    connect database to server

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,8 @@
-const User = require('../models/userModel.js');
+// const User = require('../models/userModel.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
+const db = require('../queries')
 
 
 const secretKey='This-is-a-secret-key';
@@ -16,21 +18,71 @@ const createErr = (errInfo) => {
 
 const userController = {};
 
-userController.createUser = (req, res, next) => {
+// userController.createUser = (req, res, next) => {
 
-}
+//   //waiting on oauth..
+
+// }
 
 userController.verifyUser = (req,res,next) => {
-  // req.body will contain information about the username and password 
-  // obj deconstruct the username and password, check with jasmine about the password
-  // check to see if the username and password match what's in the database
+  // req.body will contain information about the username, id, access id?
+  // obj deconstruct the body
+  // check to see if the informations matches what's in the database
   // do this by querying the database for username and check if the password exists
   // if it exists send a message through res.locals to say verified user
 
 }
 
-userController.getData = (req,res,next) => {
-  //
+/* 
+res.locals.data will look like
+res.locals.data = {
+  user: {
+    username: user,
+    circles:[...]
+  }
+  circles: {
+    dance:{
+      name: dance,
+      members: [...]
+      Events: {
+        name: [...]
+      }
+    }
+  events: {
+    event1: {event info},
+    event2: {event info}
+  }å
+}
+
+*/
+userController.getUser = (req,res,next) => {
+  const {_id} = req.params;
+  console.log('this is the id', _id)
+  // create a string that will query the data
+
+  const qString = `
+    select * 
+    from circles.users a
+    where a.id = 1 
+    limit 4;
+    `;
+    db.query(qString)
+    .then((data)=> {
+      console.log('this is our data, ',data)
+      res.locals.user = data.rows;
+      return next()
+    })
+  //req.params will contain information on the user's id
+  // may need multiple querys to get info from database
+  // query the database per the ID and use join tables
+  // throw into res.locals.data after manipulation
+
+}
+
+userController.deleteUser = (req, res, next) => {
+  // takes the req.params and querys the database for the user and then removes them
+  // const {id} = req.params
+  const qString = ''
 
 }
 
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,29 +1,28 @@
-
 const express = require('express');
-
+const userController = require('../controllers/userController')
 
 const userRouter = express.Router();
 
-// save a new user
-userRouter.post('/signup',
-  userController.createUser,
-	cookieController.setSSIDCookie, //Set JWT token when a new user signs up
-  (req, res) => {
-    console.log('--Sending data from userRouter.POST\'s aynonmouns func--');
-    return res.status(200).json(res.locals);
-  }
-);
+// // save a new user
+// userRouter.post('/signup',
+//   userController.createUser,
+// 	cookieController.setSSIDCookie, //Set JWT token when a new user signs up
+//   (req, res) => {
+//     console.log('--Sending data from userRouter.POST\'s aynonmouns func--');
+//     return res.status(200).json(res.locals);
+//   }
+// );
 
-//verify login info
-userRouter.post('/login',
-    userController.verifyUser,
-		// cookieController.setSSIDCookie, //Set JWT token when a new user signs up
+// //verify login info
+// userRouter.post('/login',
+//     userController.verifyUser,
+// 		// cookieController.setSSIDCookie, //Set JWT token when a new user signs up
 
-    (req, res) => {
-    console.log('--Sending data from userRouter.GET\'s aynonmouns func--');
-    return res.status(200).json(res.locals); 
-    }
-);
+//     (req, res) => {
+//     console.log('--Sending data from userRouter.GET\'s aynonmouns func--');
+//     return res.status(200).json(res.locals); 
+//     }
+// );
 
 // get a user's info
 userRouter.get('/:_id',
@@ -35,25 +34,14 @@ userRouter.get('/:_id',
     }
 );
 
-
-
-// // update the trip's information
-// router.patch('/:_id',
-//   // middleware
+// // delete user
+// userRouter.delete('/:_id',1
+//   userController.deleteUser,
 //   (req, res) => {
-//     console.log('--Sending data from userRouter.PATCH\'s aynonmouns func--');
-//     return res.status(200).json(); //
+//     console.log('--Sending data from charaRouter.DELETE\'s aynonmouns func--');
+//     return res.status(200).json(res.locals.deletedCharacter); // We need to send back the updated character's object (so the client can re-render)
 //   }
 // );
-
-// delete user
-userRouter.delete('/:_id',
-  userController.deleteUser,
-  (req, res) => {
-    console.log('--Sending data from charaRouter.DELETE\'s aynonmouns func--');
-    return res.status(200).json(res.locals.deletedCharacter); // We need to send back the updated character's object (so the client can re-render)
-  }
-);
 
 // EXPORT THE ROUTER!!!
 module.exports = userRouter;
