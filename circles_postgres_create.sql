6f6eef247456d519d4e018460f21cc90de92971e
Jasmine <jasminee@gmail.com>
2023-05-23 09:37:26 -0400

    added insert data for script

--- circles_postgres_create.sql
+++ circles_postgres_create.sql
@@ -53,3 +53,78 @@ ALTER TABLE circles.events ADD CONSTRAINT fk_events_circles FOREIGN KEY ( circle
 COMMENT ON COLUMN circles.events.daypart IS 'can be morning, afternoon, evening, or null';
 
 COMMENT ON COLUMN circles.events.note IS 'optional note for an event';
+
+
+ INSERT INTO circles.circles VALUES (1, 'Climb time!');
+ INSERT INTO circles.circles VALUES (1, 'Hack Hour');
+
+ INSERT INTO circles.users VALUES (1, 'jnoor', 'Jasmine', 'user1@gmail.com');
+ INSERT INTO circles.users VALUES (2, 'jdonato', 'John', 'user2@gmail.com');
+ INSERT INTO circles.users VALUES (3, 'wmurphy', 'Billy', 'user3@gmail.com');
+ INSERT INTO circles.users VALUES (4, 'nly', 'Nicky', 'user4@gmail.com');
+ INSERT INTO circles.users VALUES (5, 'batman', 'Kelvin', 'user5@gmail.com');
+ INSERT INTO circles.users VALUES (6, 'cashley', 'Christian', 'user6@gmail.com');
+ INSERT INTO circles.users VALUES (7, 'ma', 'Michael Angelo', 'user7@gmail.com');
+ INSERT INTO circles.users VALUES (8, 'nz', 'Ngoc', 'user8@gmail.com');
+
+ INSERT INTO circles.users VALUES (2, 1);
+ INSERT INTO circles.users VALUES (2, 2);
+ INSERT INTO circles.users VALUES (2, 3);
+ INSERT INTO circles.users VALUES (2, 4);
+ INSERT INTO circles.users VALUES (2, 5);
+ INSERT INTO circles.users VALUES (2, 6);
+ INSERT INTO circles.users VALUES (2, 7);
+ INSERT INTO circles.users VALUES (2, 8);
+ INSERT INTO circles.users VALUES (2, 9);
+ INSERT INTO circles.users VALUES (1, 1);
+ INSERT INTO circles.users VALUES (1, 2);
+ INSERT INTO circles.users VALUES (1, 3);
+ INSERT INTO circles.users VALUES (1, 4);
+
+ INSERT INTO circles.events VALUES (1, 1, '2023-05-27', 'evening', '');
+ INSERT INTO circles.events VALUES (2, 1, '2023-05-29', 'morning', 'lead climbing');
+ INSERT INTO circles.events VALUES (3, 1, '2023-05-30', 'evening', '');
+ INSERT INTO circles.events VALUES (4, 1, '2023-06-02', 'evening', '');
+ INSERT INTO circles.events VALUES (5, 1, '2023-06-05', 'afternoon', 'top rope');
+ INSERT INTO circles.events VALUES (6, 2, '2023-05-28', 'evening', '');
+ INSERT INTO circles.events VALUES (7, 2, '2023-05-29', 'morning', 'SDI');
+ INSERT INTO circles.events VALUES (8, 2, '2023-06-03', 'morning', '');
+ INSERT INTO circles.events VALUES (9, 2, '2023-06-04', 'evening', '');
+
+ INSERT INTO circles.event_users VALUES (1, 1);
+ INSERT INTO circles.event_users VALUES (1, 2);
+ INSERT INTO circles.event_users VALUES (2, 2);
+ INSERT INTO circles.event_users VALUES (3, 3);
+ INSERT INTO circles.event_users VALUES (3, 4);
+ INSERT INTO circles.event_users VALUES (4, 1);
+ INSERT INTO circles.event_users VALUES (4, 2);
+ INSERT INTO circles.event_users VALUES (4, 3);
+ INSERT INTO circles.event_users VALUES (4, 4);
+ INSERT INTO circles.event_users VALUES (5, 4);
+ INSERT INTO circles.event_users VALUES (6, 7);
+ INSERT INTO circles.event_users VALUES (6, 8);
+ INSERT INTO circles.event_users VALUES (6, 1);
+ INSERT INTO circles.event_users VALUES (6, 1);
+ INSERT INTO circles.event_users VALUES (6, 1);
+ INSERT INTO circles.event_users VALUES (6, 1);
+ INSERT INTO circles.event_users VALUES (6, 1);
+ INSERT INTO circles.event_users VALUES (6, 1);
+ INSERT INTO circles.event_users VALUES (6, 1);
+ INSERT INTO circles.event_users VALUES (6, 1);
+ INSERT INTO circles.event_users VALUES (6, 2);
+ INSERT INTO circles.event_users VALUES (6, 3);
+ INSERT INTO circles.event_users VALUES (6, 4);
+ INSERT INTO circles.event_users VALUES (6, 5);
+ INSERT INTO circles.event_users VALUES (6, 6);
+ INSERT INTO circles.event_users VALUES (6, 7);
+ INSERT INTO circles.event_users VALUES (6, 8);
+ INSERT INTO circles.event_users VALUES (6, 9);
+ INSERT INTO circles.event_users VALUES (7, 1);
+ INSERT INTO circles.event_users VALUES (7, 3);
+ INSERT INTO circles.event_users VALUES (7, 5);
+ INSERT INTO circles.event_users VALUES (7, 7);
+ INSERT INTO circles.event_users VALUES (7, 9);
+ INSERT INTO circles.event_users VALUES (8, 1);
+ INSERT INTO circles.event_users VALUES (8, 2);
+ INSERT INTO circles.event_users VALUES (8, 3);
+ INSERT INTO circles.event_users VALUES (9, 5);
