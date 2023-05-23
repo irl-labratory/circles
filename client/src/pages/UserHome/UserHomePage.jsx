241b40f250c16ebb892dbc6b2fc5175d9da5f45f
William Murphy <williamjmurphy@gmial.com>
2023-05-23 17:56:51 -0500

    base styling for calendar is done

--- client/src/pages/UserHome/UserHomePage.jsx
+++ client/src/pages/UserHome/UserHomePage.jsx
@@ -3,7 +3,7 @@ import { useNavigate, useParams, useLoaderData, json } from 'react-router-dom';
 import { mainContext } from '../../context';
 import { toCamelCase, generateEventColors, renderEvents } from './helperFunc';
 import Calendar from 'react-calendar';
-// import 'react-calendar/dist/Calendar.css';
+
 
 
 const svg = <svg fill="#000000" height="22px" width="25px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 480.3 480.3" xmlSpace="preserve"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M254.15,234.1V13.5c0-7.5-6-13.5-13.5-13.5s-13.5,6-13.5,13.5v220.6c-31.3,6.3-55,34-55,67.2s23.7,60.9,55,67.2v98.2 c0,7.5,6,13.5,13.5,13.5s13.5-6,13.5-13.5v-98.2c31.3-6.3,55-34,55-67.2C309.15,268.2,285.55,240.4,254.15,234.1z M240.65,342.8 c-22.9,0-41.5-18.6-41.5-41.5s18.6-41.5,41.5-41.5s41.5,18.6,41.5,41.5S263.55,342.8,240.65,342.8z"></path> <path d="M88.85,120.9V13.5c0-7.5-6-13.5-13.5-13.5s-13.5,6-13.5,13.5v107.4c-31.3,6.3-55,34-55,67.2s23.7,60.9,55,67.2v211.4 c0,7.5,6,13.5,13.5,13.5s13.5-6,13.5-13.5V255.2c31.3-6.3,55-34,55-67.2S120.15,127.2,88.85,120.9z M75.35,229.6 c-22.9,0-41.5-18.6-41.5-41.5s18.6-41.5,41.5-41.5s41.5,18.6,41.5,41.5S98.15,229.6,75.35,229.6z"></path> <path d="M418.45,120.9V13.5c0-7.5-6-13.5-13.5-13.5s-13.5,6-13.5,13.5v107.4c-31.3,6.3-55,34-55,67.2s23.7,60.9,55,67.2v211.5 c0,7.5,6,13.5,13.5,13.5s13.5-6,13.5-13.5V255.2c31.3-6.3,55-34,55-67.2S449.85,127.2,418.45,120.9z M404.95,229.6 c-22.9,0-41.5-18.6-41.5-41.5s18.6-41.5,41.5-41.5s41.5,18.6,41.5,41.5S427.85,229.6,404.95,229.6z"></path> </g> </g> </g></svg>
@@ -37,12 +37,9 @@ const UserHomePage = () => {
         setShowPopup(true);
     };
 
-
     // When you clink on this date div
     const onChange = (date) => {
         setDate(date);
-        console.log(JSON.stringify(date)) // -> Mon May 22 2023 00:00:00 GMT-0500 (CDT)
-        console.log(typeof date)
       };
 
     const eventColors = generateEventColors(mainObjData)
@@ -57,19 +54,19 @@ const UserHomePage = () => {
             return eventDate.getTime() === formattedDate.getTime(); // Compare date objects
           });
           
-          console.log(foundEvent)
           if (foundEvent) {
      
             const circleColor = eventColors[foundEvent.circle];
+            console.log('circleColor',circleColor)
             return (
               <div className={`event-tile ${circleColor}`}>
-                {foundEvent.eventName}
+                {foundEvent.eventName} - attending: {foundEvent.going.length}
                 <div className="event-dot" style={{ backgroundColor: circleColor }} />
               </div>
             );
           }
         }
-    
+
         return null;
       };
      
@@ -80,13 +77,11 @@ const UserHomePage = () => {
             <div className='settings'>
                 <button onClick={handleSettings} className='settings-btn'>{svg}</button>
             </div>
-            <div className="react-calendar">
                 <Calendar   onChange={onChange} 
                             value={date} 
                             tileContent={tileContent}
                             onClickDay={handleDayClick}
                                 />
-            </div>
         </div>
     )
 }
